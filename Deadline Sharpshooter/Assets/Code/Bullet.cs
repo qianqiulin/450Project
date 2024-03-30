9d50eb4ce42cf8f56d0b599c981228a4481584f3
tedkim12 <k.tedsungtae@wustl.edu>
2024-03-29 19:35:44 -0500

    Firework effect added when obstacles gets shooted

--- Deadline Sharpshooter/Assets/Code/Bullet.cs
+++ Deadline Sharpshooter/Assets/Code/Bullet.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float life = 3;
+    public GameObject fireworkPrefab;
     void Awake()
     {
         Destroy(gameObject, life);
@@ -13,6 +14,7 @@ public class Bullet : MonoBehaviour
     {
         if(collision.gameObject.tag != "Boundary" || collision.gameObject.tag != "Despawner") {
             Destroy(collision.gameObject);
+            GameObject firework=Instantiate(fireworkPrefab, collision.transform.position, Quaternion.identity);
             // Notify GameManager to update the score
             Obstacles obstacle = collision.gameObject.GetComponent<Obstacles>();
             if (obstacle != null) // Ensure the collided object is an obstacle
@@ -20,6 +22,7 @@ public class Bullet : MonoBehaviour
                 // Notify GameManager to update the score with the obstacle's score value
                 GameManager.instance.AddScore(obstacle.scoreValue);
             }
+            Destroy(firework, 1f);
         }
         
         Destroy(gameObject);
