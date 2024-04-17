d373e76a382c2aa00195894cf4b693209703e502
qianqiulin <p.qian@wustl.edu>
2024-04-16 19:42:42 -0500

    reload cartoon finished

---
+++ Deadline Sharpshooter/Assets/Code/reloadForBoss.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class reloadForBoss : MonoBehaviour
+{
+    public ShooterforBossScene shooterController; // Assign this in the Inspector
+    private Animator otherAnimator;
+
+    void Start()
+    {
+        otherAnimator = GetComponent<Animator>();
+        print("reloadCartoonworkings");
+    }
+    //nothing here
+    void Update()
+    {
+        if (shooterController != null)
+        {
+            // Get the current bullet count from the ShooterController
+            int bullets = shooterController.GetCurrentBullets();
+
+            // Set the Animator parameter (assuming it's an integer parameter named "BulletCount")
+            otherAnimator.SetInteger("bulletnumber", bullets);
+            //print(bullets);
+        }
+    }
+}
